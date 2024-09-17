2bc772f817a483ee77f7f94faa3c306ead8e1815
Rwilliamsc <rwilliam.sc@gmail.com>
2024-09-17 19:30:31 -0300

    feat: create log

--- src/main/java/infnet/edu/oauthms/service/AuthService.java
+++ src/main/java/infnet/edu/oauthms/service/AuthService.java
@@ -3,6 +3,8 @@ package infnet.edu.oauthms.service;
 import infnet.edu.oauthms.client.UserClient;
 import infnet.edu.oauthms.dto.UserResponseDTO;
 import infnet.edu.oauthms.security.JwtTokenProvider;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,13 +17,22 @@ public class AuthService {
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
 
+    Logger logger= LogManager.getLogger(AuthService.class);
+
     public String authenticator(String username, String password) {
-        UserResponseDTO userResponse = userClient.validateUser(username, password);
+        try{
+            logger.info("Authenticating user {}", username);
+            UserResponseDTO userResponse = userClient.validateUser(username, password);
 
-        if(userResponse != null) {
-            return jwtTokenProvider.createToken(userResponse);
+            if(userResponse != null) {
+                return jwtTokenProvider.createToken(userResponse);
+            }
+
+            throw new RuntimeException("Invalid Credentils");
         }
-
-        throw new RuntimeException("Invalid Credentils");
+        catch(Exception e){
+            logger.error(e.getMessage());
+            throw new RuntimeException("Error authenticating user", e);
+        }
     }
 }
